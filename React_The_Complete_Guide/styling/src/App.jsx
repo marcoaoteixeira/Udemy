c7bf122119422e935df4001cf9ec5a0a27e5dfff
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-03-03 16:27:28 +0000

    React: Styling with styled-components

---
+++ React_The_Complete_Guide/styling/src/App.jsx
@@ -0,0 +1,13 @@
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Auth />
+      </main>
+    </>
+  );
+};
