3d79a73988219adbe6c29473a73617b1a68d52eb
Marco Teixeira <marcoaoteixeira@gmail.com>
2024-03-03 17:20:26 +0000

    React: using Tailwind to styling app.

---
+++ React_The_Complete_Guide/tailwind_sample/src/components/Input.jsx
@@ -0,0 +1,18 @@
+export default function Input({ label, invalid, ...props }) {
+  const labelClass = `block mb-2 text-xs font-bold tracking-wide uppercase ${
+    invalid ? "text-red-400" : "text-stone-300"
+  }`;
+
+  const inputClass = `w-full px-3 py-2 leading-tight border rounded shadow ${
+    invalid
+      ? "text-red-500 bg-red-100 border-red-300"
+      : "text-gray-700 bg-stone-300"
+  }`;
+
+  return (
+    <p>
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...props} />
+    </p>
+  );
+}
